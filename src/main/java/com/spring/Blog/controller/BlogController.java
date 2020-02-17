80812f94b3533227c66256bd56ff36377ddadbb9
Jean Lima <jean.jr.souza@hotmail.com>
2020-02-17 18:37:02 -0300

    Criação de metodo e template para mostrar post por id

--- src/main/java/com/spring/Blog/controller/BlogController.java
+++ src/main/java/com/spring/Blog/controller/BlogController.java
@@ -4,6 +4,7 @@ import com.spring.Blog.model.Post;
 import com.spring.Blog.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,6 +23,14 @@ public class BlogController {
         List<Post> posts = blogService.findAll();
         mv.addObject("posts", posts);
     return mv;
+    }
+
+    @RequestMapping (value = "/posts/{id}", method = RequestMethod.GET)
+    public ModelAndView getPostdetails(@PathVariable("id") long id){
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = blogService.findById(id);
+        mv.addObject("post", post);
+        return mv;
     }
 
 }
